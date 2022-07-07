fef2385336746c94ad1eb044125ef6c86993590e
Refansyach <75077651+AuroraLeafa@users.noreply.github.com>
2022-07-07 17:06:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.navbar');
+
+window.onscroll = () => {
+    if (window.scrollY > 200) {
+        nav.classList.add('nav-active');
+    } else {
+        nav.classList.remove('nav-active');
+    }
+};
